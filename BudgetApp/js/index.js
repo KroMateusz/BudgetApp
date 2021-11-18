d08671a9cc72db5852084a004b651bc09c31c19d
KroMateusz <81087684+KroMateusz@users.noreply.github.com>
2021-11-18 02:14:04 +0100

    Update index.js

--- BudgetApp/js/index.js
+++ BudgetApp/js/index.js
@@ -10,7 +10,8 @@ class BudgetApp {
     totalBudgetInfo = null;
     error = null;
 
-    totaBudget = null;
+    totalBudget = null;
+    editedItem = null;
 
     currency = 'PLN';
 
@@ -47,9 +48,17 @@ class BudgetApp {
         this.error = document.querySelector(this.UiSelectors.error);
 
         this.addEventListeners();
+        this.updateTotalBudget();
+        this.toggleListVisibility();
     }
     addEventListeners() {
         this.enterButton.addEventListener('click', () => this.addItem());
+        this.descriptionInput.addEventListener('blur', () => this.hideError());
+        this.valueInput.addEventListener('blur', () => this.hideError());
+
+        this.balanceList.addEventListener('click', (e) => {
+            this.listClickHandler(e.target)
+        })
 
         document.addEventListener('keyup', (e) => {
             if (e.key === 'Enter') {
@@ -70,10 +79,38 @@ class BudgetApp {
 
         this.balanceItems.push(newItem);
 
-        element.insertAdjacentHTML('beforeend', this.createItem(newItem.id, newItem.isPlus, newItem.description, newItem.value));
+        element.insertAdjacentHTML('beforeend', this.createItem(newItem.id, newItem.isPlus, newItem.description, newItem.value, ), );
 
         this.updateTotalBudget();
         this.numberOfItems++;
+        this.toggleListVisibility();
+    }
+
+    deleteItem(target) {
+        const {
+            element,
+            id
+        } = this.getListElement(target);
+        const items = [...this.balanceItems];
+
+        this.balanceItems = items.filter((item) => item.id !== id);
+
+        this.updateTotalBudget();
+        element.remove()
+        this.toggleListVisibility();
+    }
+
+    editItem() {
+        const {
+            element,
+            id
+        } = this.getListElement(target);
+        this.editedItem = element;
+        const selectedElement = this.balanceItems.find(item => item.id === id);
+
+        this.descriptionInput.value = selectedElement.description;
+        this.valueInput.value = selectedElement.value;
+        this.switchInput.checked = !selectedElement.isPlus;
     }
 
     createItem(id, isPositive, description, price) {
@@ -87,6 +124,24 @@ class BudgetApp {
     </li>`
     }
 
+    listClickHandler(target) {
+        if (target.dataset && target.dataset.editButton !== undefined) {
+            this.editItem(target);
+        }
+        if (target.dataset && target.dataset.deleteButton !== undefined) {
+            this.deleteItem(target);
+        }
+    }
+    getListElement(target) {
+        const listElement = target.parentElement.parentElement;
+        const listElementId = listElement.id
+
+        return {
+            element: listElement,
+            id: listElementId,
+        }
+    }
+
     getInputValues() {
         const isPlus = !this.switchInput.checked;
         const description = this.descriptionInput.value;
@@ -94,7 +149,7 @@ class BudgetApp {
 
         if (value > 0 && description) {
             return {
-                id: this.numberOfItems,
+                id: `${this.numberOfItems}`,
                 isPlus,
                 description,
                 value,
@@ -104,15 +159,23 @@ class BudgetApp {
     }
 
     updateTotalBudget() {
-        this.totaBudget = 0;
+        this.totalBudget = 0;
         this.balanceItems.forEach(({
             isPlus,
             value
         }) => {
-            isPlus ? (this.totaBudget += parseFloat(value)) : (this.totalBudget -= parseFloat(value))
+            isPlus ? (this.totalBudget += parseFloat(value)) : (this.totalBudget -= parseFloat(value))
         })
-        this.totalBudgetInfo.innerHtml = `Your total budget is <span class="${this.totalBudget >= 0 ? 'balance__heading--poisitive' : 'balance__heading--negative'}">${this.formatPrice(Math.abs(this.totalBudget), this.totalBudget >= 0 ? true : false)}</span>`
+        this.totalBudgetInfo.innerHTML = `
+                    Your total budget is <span class = "${this.totalBudget >= 0 ? 'balance__heading--positive' : 'balance__heading--negative'}"> ${this.formatPrice(Math.abs(this.totalBudget), this.totalBudget >= 0 ? true : false)
+                    } </span>`
     }
+
+    toggleListVisibility() {
+        this.balanceListExpenses.children.length ? this.balanceListExpenses.parentElement.classList.remove('hide') : this.balanceListExpenses.parentElement.classList.add('hide');
+        this.balanceListIncomes.children.length ? this.balanceListIncomes.parentElement.classList.remove('hide') : this.balanceListIncomes.parentElement.classList.add('hide');
+    }
+
     formatPrice(price, isPositive) {
         return `${isPositive ? '+' : '-'} ${this.setNumberOfDigits(price, 2)} ${this.currency}`
     }
